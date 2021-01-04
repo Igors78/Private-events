629d1b9aca10558f50ed14e06dc2d53e31864544
Igors78 <oleinikovsigors@gmail.com>
2021-01-04 14:59:17 +0200

    Add user and authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_190729) do
+ActiveRecord::Schema.define(version: 2021_01_04_125535) do
 
   create_table "events", force: :cascade do |t|
     t.text "description"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2021_01_03_190729) do
     t.datetime "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "events", "users"
 end
