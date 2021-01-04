5375a3685279852b872449fd4cf7e0be2e6895fd
Igors78 <oleinikovsigors@gmail.com>
2021-01-04 13:42:43 +0200

    Fix rubocop errors

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -17,7 +17,9 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should create event' do
     assert_difference('Event.count') do
-      post events_url, params: { event: { date: @event.date, description: @event.description, location: @event.location } }
+      post events_url, params: { event: { date: @event.date,
+                                          description: @event.description,
+                                          location: @event.location } }
     end
 
     assert_redirected_to event_url(Event.last)
@@ -34,7 +36,9 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should update event' do
-    patch event_url(@event), params: { event: { date: @event.date, description: @event.description, location: @event.location } }
+    patch event_url(@event), params: { event: { date: @event.date,
+                                                description: @event.description,
+                                                location: @event.location } }
     assert_redirected_to event_url(@event)
   end
 
