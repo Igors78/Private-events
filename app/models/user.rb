721dcc98774d94efd53447c4474b23f6da14b36c
Igors78 <oleinikovsigors@gmail.com>
2021-01-05 16:25:26 +0200

    Add attendance model for keeping track of attendance

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   has_many :events, inverse_of: :creator, dependent: :destroy
   has_many :invites, inverse_of: :invited_user
   has_many :invited_to_events, through: :invites, class_name: 'Event'
+  has_many :attendances, inverse_of: :attendee
+  has_many :attended_events, through: :attendances, class_name: 'Event'
   validates :name, presence: true, uniqueness: true
 end
