2c9756f94eef37dd26d0df245195ae312080ea08
Igors78 <oleinikovsigors@gmail.com>
2021-01-05 08:26:34 +0200

    Generate invite model to associate user and event through

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :events, inverse_of: :creator, dependent: :destroy
+  has_many :invites, inverse_of: :invited_user
+  has_many :invited_to_events, through: :invites, class_name: 'Event'
+  validates :name, presence: true, uniqueness: true
 end
