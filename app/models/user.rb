0052e65f6fcdab82748e28251277ea0afac37590
Igors78 <oleinikovsigors@gmail.com>
2021-01-06 21:23:19 +0200

    Add user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :events, inverse_of: :creator, dependent: :destroy
-  has_many :invites, inverse_of: :invited_user
+  has_many :invites, inverse_of: :invited_user, dependent: :destroy
   has_many :invited_to_events, through: :invites, class_name: 'Event'
-  has_many :attendances, inverse_of: :attendee
+  has_many :attendances, inverse_of: :attendee, dependent: :destroy
   has_many :attended_events, through: :attendances, class_name: 'Event'
   validates :name, presence: true, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'Example User')
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
+  test 'name should be present' do
+    @user.name = '     '
+    assert_not @user.valid?
+  end
+
+  test 'user name should be unique' do
+    duplicate_user = @user.dup
+
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test 'associated events should be destroyed' do
+    @user.save
+    @user.events.create!(description: 'Lorem ipsum', location: 'Moscow', date: '2021-01-03 21:07:29')
+    assert_difference 'Event.count', -1 do
+      @user.destroy
+    end
+  end
 end
