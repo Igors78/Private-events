780bb5fef76fc765b8824c022a9c5c1262bff5e9
CornerstoneII <seunmyles@gmail.com>
2021-01-07 09:45:51 +0100

    Move logic from the views to the helpers file

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,24 @@
 module UsersHelper
+  def check_user
+    #     if current_user == @event.creator
+    #       content_tag(:div,
+    #                   link_to('Invite People', attendances_path(event_id: @event.id),
+    #                           class: 'btn btn-primary text-uppercase'))
+    #     elsif @is_attendy
+    #       content_tag(:div,
+    #                   link_to('Withdraw', attendances_path(event_id: @event.id), method: :patch,
+    #                                                                              class: 'btn btn-primary text-uppercase'))
+    #     else
+    #       content_tag(:div,
+    #                   link_to('Join Event', attendances_path(event_id: @event.id), method: :patch,
+    #                                                                                class: 'btn btn-primary text-uppercase'))
+    #     end
+
+    if current_user?(@user)
+      content_tag(:a,
+                  link_to("Delete #{@user.name}", @user, method: :delete,
+                                                         data: { confirm: 'You sure?' },
+                                                         class: 'badge bg-secondary'))
+    end
+  end
 end
