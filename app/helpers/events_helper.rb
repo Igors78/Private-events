00d08856b10da64f0b1daf8487a61402c31ce6b9
CornerstoneII <seunmyles@gmail.com>
2021-01-08 10:13:48 +0100

    Move logic out of events show page to events_helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,12 @@
 module EventsHelper
+  def rsvp_user
+    render inline:'
+    <% if @event.invited_users.any? %>
+        <strong>Users invited:</strong> <br>
+        <% @event.invited_users.each do |user| %>
+        <%= user.name %>
+        <% end %>
+    <% end %>
+'
+  end
 end
