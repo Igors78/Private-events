249bf62183393a65a13d80f228b2948a79f9fe86
Igors78 <oleinikovsigors@gmail.com>
2021-01-09 22:38:44 +0200

    Remove logic from views

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -19,4 +19,43 @@ module EventsHelper
         <% end %>
     <% end %>'
   end
+
+  def handling_links
+    render inline: '
+    <% if current_user?(@event.creator) %>
+      <%= link_to "Delete", @event, method: :delete,
+                                data: { confirm: "You sure?" },
+                                class: "badge bg-warning" %>
+      <%= link_to "Edit", edit_event_path(@event), class: "badge bg-warning" %>
+      <% end %>
+      <%= link_to "Back", events_path, class: "badge bg-warning" %>
+    '
+  end
+
+  def user_links
+    render inline: '
+    <%  @users.each do |user|%>
+      <li><%= link_to user.name, user %>
+
+          <% if current_user?(@event.creator) && user != current_user %>
+          <%if @event.invited_users.include?(user) %>
+          <%= link_to "Dismiss",
+                          { controller: "events", action: "dismiss_user",
+                          user_id: user.id },
+                          class:"badge bg-danger" %>
+          <% elsif @event.attendees.include?(user)%>
+          <%= link_to "Dismiss invite",
+                          { controller: "users", action: "dismiss_invite",
+                          user_id: user.id },
+                          class:"badge bg-danger" %>
+          <%else%>
+          <%= link_to "Invite",
+                          { controller: "events", action: "invite_user",
+                          user_id: user.id },
+                          class:"badge bg-secondary" %>
+          <%end%>
+          <% end %></li>
+      <% end %>
+    '
+  end
 end
